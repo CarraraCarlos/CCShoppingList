7db572c4c1f239c26825278f603485cef9b55e3e
Carlos Carrara <carlinhos.alberto308@gmail.com>
2025-05-12 11:39:55 -0300

    commit * Alocamento Placeholder v2 * InputContainer add *  TouchableOpacity ON

--- CCShoppingList/app/home.jsx
+++ CCShoppingList/app/home.jsx
@@ -1,5 +1,5 @@
 import React from 'react'
-import { SafeAreaView, StyleSheet, Text, View, ImageBackground } from 'react-native'
+import { SafeAreaView, StyleSheet, Text, View, ImageBackground, TextInput, TouchableWithoutFeedback, TouchableOpacity } from 'react-native'
 import { Ionicons } from '@expo/vector-icons';
 
 
@@ -20,7 +20,17 @@ export default function Home()
         <View style={{ flex: 1 }}></View>
 
         <View style={styles.footer}>
-          
+          <View style={styles.inputContainer}>
+            <TextInput
+            color="#fff"
+            fontSize={18}
+            placeholderTextColor="#aeaeae"
+            placeholder='Digite o nome do produto...'
+            />
+          </View>
+          <TouchableOpacity style={styles.iconContainer}>
+            <Ionicons name="add" size={36} color="#fff"/>
+          </TouchableOpacity>
         </View>
      </ImageBackground>
     </SafeAreaView>
@@ -42,5 +52,33 @@ const styles = StyleSheet.create({
     fontSize: 26,
     fontWeight: 'bold',
     color: '#fff'
+  },
+  footer: {
+    backgroundColor: '#000000c0',
+    position: 'absolute',
+    bottom: 0,
+    width: '100%',
+    flexDirection: 'row',
+    alignItems: 'center',
+    paddingHorizontal: 20,
+    borderTopLeftRadius: 30,
+    borderTopRightRadius: 30,
+  },
+  inputContainer: {
+    flex: 1,
+    backgroundColor: "#000",
+    height: 50,
+    marginVertical: 20,
+    borderRadius: 30,
+    paddingHorizontal: 20,
+    justifyContent: 'center',
+  },
+  iconContainer: {
+    height: 50,
+    width: 50,
+    backgroundColor: '#000',
+    borderRadius: 25,
+    justifyContent: 'center',
+    alignItems: 'center',
   }
 })
